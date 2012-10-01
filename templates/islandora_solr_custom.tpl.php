306bca4fcc74d1d5c4594ee13ec2f6f87f28496c
Nigel Banks <nigel.g.banks@gmail.com>
2012-10-01 07:08:55 +0200

    Changed the display of solr results to reflect the new naming convention.

--- templates/islandora_solr_custom.tpl.php
+++ templates/islandora_solr_custom.tpl.php
@@ -11,97 +11,87 @@
  * - $style: the style of the display ('div' or 'table'). Set in admin page by default. Overridden by the query value: ?display=foo
  * - $results: the array containing the solr search results
  * - $table_rendered: If the display style is set to 'table', this will contain the rendered table.
- *    For theme overriding, see: theme_islandora_solr_custom_table() 
+ *    For theme overriding, see: theme_islandora_solr_custom_table()
  * - $switch_rendered: The rendered switch to toggle between display styles
- *    For theme overriding, see: theme_islandora_solr_custom_switch() 
+ *    For theme overriding, see: theme_islandora_solr_custom_switch()
  *
  */
 ?>
 <?php print $switch_rendered; ?>
-
 <?php if ($style == 'div'): ?>
   <ol class="islandora_solr_results" start="<?php print $record_start; ?>">
     <?php if ($results == ''): print '<p>' . t('Your search yielded no results') . '</p>'; ?>
     <?php else: ?>
       <?php foreach ($results as $id => $result): ?>
         <li class="islandora_solr_result">
-          
           <?php $pid_link = 'fedora/repository/' . $result['PID']['value']; ?>
-          
-          <?php if (!empty($result['dc_title_s']['value'])): ?>
-            <div class="solr-field <?php print $result['dc_title_s']['class']; ?>">
-              <div class="label"><label><?php print t($result['dc_title_s']['label']); ?>:</label></div>
-              <div class="value"><?php print l($result['dc_title_s']['value'], $pid_link); ?></div>
+          <?php if (!empty($result['DC.content.title_s']['value'])): ?>
+            <div class="solr-field <?php print $result['DC.content.title_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['DC.content.title_s']['label']); ?>:</label></div>
+              <div class="value"><?php print l($result['DC.content.title_s']['value'], $pid_link); ?></div>
             </div>
           <?php endif; ?>
-          <?php if (!empty($result['eac_topic_s']['value'])): ?>
-            <div class="solr-field <?php print $result['eac_topic_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['eac_topic_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['eac_topic_s']['value']; ?></div>
+          <?php if (!empty($result['EAC-CPF.content.topic_s']['value'])): ?>
+            <div class="solr-field <?php print $result['EAC-CPF.content.topic_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['EAC-CPF.content.topic_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['EAC-CPF.content.topic_s']['value']; ?></div>
             </div>
           <?php endif; ?>
-          <?php if (!empty($result['eac_place_s']['value'])): ?>
-            <div class="solr-field <?php print $result['eac_place_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['eac_place_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['eac_place_s']['value']; ?></div>
+          <?php if (!empty($result['EAC-CPF.content.place_s']['value'])): ?>
+            <div class="solr-field <?php print $result['EAC-CPF.content.place_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['EAC-CPF.content.place_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['EAC-CPF.content.place_s']['value']; ?></div>
             </div>
           <?php endif; ?>
-          <?php if (!empty($result['eac_person_s']['value'])): ?>
-            <div class="solr-field <?php print $result['eac_person_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['eac_person_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['eac_person_s']['value']; ?></div>
+          <?php if (!empty($result['EAC-CPF.content.person_s']['value'])): ?>
+            <div class="solr-field <?php print $result['EAC-CPF.content.person_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['EAC-CPF.content.person_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['EAC-CPF.content.person_s']['value']; ?></div>
             </div>
           <?php endif; ?>
-          <?php if (!empty($result['eac_agencyName_s']['value'])): ?>
-            <div class="solr-field <?php print $result['eac_agencyName_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['eac_agencyName_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['eac_agencyName_s']['value']; ?></div>
+          <?php if (!empty($result['EAC-CPF.content.agencyName_s']['value'])): ?>
+            <div class="solr-field <?php print $result['EAC-CPF.content.agencyName_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['EAC-CPF.content.agencyName_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['EAC-CPF.content.agencyName_s']['value']; ?></div>
             </div>
           <?php endif; ?>
-          <?php if (!empty($result['eac_corporateBody_s']['value'])): ?>
-            <div class="solr-field <?php print $result['eac_corporateBody_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['eac_corporateBody_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['eac_corporateBody_s']['value']; ?></div>
+          <?php if (!empty($result['EAC-CPF.content.corporateBody_s']['value'])): ?>
+            <div class="solr-field <?php print $result['EAC-CPF.content.corporateBody_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['EAC-CPF.content.corporateBody_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['EAC-CPF.content.corporateBody_s']['value']; ?></div>
             </div>
           <?php endif; ?>
-          <?php if (!empty($result['mods_topic_s']['value'])): ?>
-            <div class="solr-field <?php print $result['mods_topic_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['mods_topic_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['mods_topic_s']['value']; ?></div>
+          <?php if (!empty($result['MODS.content.topic_s']['value'])): ?>
+            <div class="solr-field <?php print $result['MODS.content.topic_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['MODS.content.topic_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['MODS.content.topic_s']['value']; ?></div>
             </div>
           <?php endif; ?>
 
-          <?php if (!empty($result['mods_creator_s']['value'])): ?>
-            <div class="solr-field <?php print $result['mods_creator_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['mods_creator_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['mods_creator_s']['value']; ?></div>
+          <?php if (!empty($result['MODS.content.creator_s']['value'])): ?>
+            <div class="solr-field <?php print $result['MODS.content.creator_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['MODS.content.creator_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['MODS.content.creator_s']['value']; ?></div>
             </div>
           <?php endif; ?>
-          <?php if (!empty($result['dc_description_s']['value'])): ?>
+          <?php if (!empty($result['DC.content.description_s']['value'])): ?>
           <?php
           $readmore = '';
           $max_length = 500;
-          if (strlen($result['dc_description_s']['value']) > $max_length) {
-            $result['dc_description_s']['value'] = preg_replace('/\s+?(\S+)?$/', '', substr($result['dc_description_s']['value'], 0, $max_length));
+          if (strlen($result['DC.content.description_s']['value']) > $max_length) {
+            $result['DC.content.description_s']['value'] = preg_replace('/\s+?(\S+)?$/', '', substr($result['DC.content.description_s']['value'], 0, $max_length));
             $readmore = '... (' . l(t('Read More'), $pid_link) . ')';
           }
           ?>
-            <div class="solr-field <?php print $result['dc_description_s']['class']; ?>">  
-              <div class="label"><label><?php print t($result['dc_description_s']['label']); ?>:</label></div>
-              <div class="value"><?php print $result['dc_description_s']['value'] . $readmore; ?></div>
+            <div class="solr-field <?php print $result['DC.content.description_s']['class']; ?>">
+              <div class="label"><label><?php print t($result['DC.content.description_s']['label']); ?>:</label></div>
+              <div class="value"><?php print $result['DC.content.description_s']['value'] . $readmore; ?></div>
             </div>
           <?php endif; ?>
         </li>
-
-
       <?php endforeach; ?>
     <?php endif; ?>
   </ol>
-
 <?php elseif ($style == 'table'): ?>
-
   <?php print $table_rendered; ?>
-
-  <?php
-
- endif;
+<?php endif; ?>
